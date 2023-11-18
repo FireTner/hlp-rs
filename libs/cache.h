948d5913797c5abfd13556d626f3ead2c137388d
FireTner <vladik.cerny.2007@seznam.cz>
2023-11-19 00:15:35 +0100

    cleanup of incache

--- libs/cache.h
+++ libs/cache.h
@@ -18,16 +18,15 @@ inline void clearCache() {
   memset(&cache, 0, sizeof(cache));
 }
 
-inline bool inCache(const vec value, const int depth) {
-  uint64_t pos = 0;
-  pos = _mm_crc32_u64(_mm_extract_epi64(value, 1), _mm_extract_epi64(value, 0));
+inline bool inCache(const vec key, const int depth) {
+  uint64_t pos = _mm_crc32_u64(_mm_extract_epi64(key, 1), _mm_extract_epi64(key, 0));
 
   pos &= cacheMask;
-  
-  if(cache[pos].depth <= depth && vec_equal(cache[pos].key, value)) return true;
+
+  if(cache[pos].depth <= depth && vec_equal(cache[pos].key, key)) return true;
 
   cache[pos].depth = depth;
-  cache[pos].key = value;
+  cache[pos].key = key;
 
   return false;
 }
