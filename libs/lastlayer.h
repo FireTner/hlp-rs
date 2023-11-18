cd0e6afa8e81c6f53cdb40eb8f1e2aa4f73ebe5f
FireTner <vladik.cerny.2007@seznam.cz>
2023-11-19 00:14:48 +0100

    minor optimization to last layer

--- libs/lastlayer.h
+++ libs/lastlayer.h
@@ -4,9 +4,8 @@
 #include "genlut.h"
 
 inline bool lastLayer(const vec input, const int prevIndex) {
-  for(int i = 0; i < indexTableSize[prevIndex]; i++) {
-    const int index = indexTable[prevIndex][i];
-    const vec output = vec_shuffle(layer[index], input);
+  for(int i = 0; i < layerSize; i++) {
+    const vec output = vec_shuffle(layer[i], input);
     
     if(vec_equal(output, goal)) return true;
   }
